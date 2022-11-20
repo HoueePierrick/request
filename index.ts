7af7b623c794e6409c04bd39790b7a1805d34581
Pierrick Houée <houee.pierrick@gmail.com>
2022-11-20 22:17:59 +0100

    WIP

--- index.ts
+++ index.ts
@@ -1,12 +1,26 @@
 // Used to download pages
 import request from "request-promise";
 import cheerio from "cheerio";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import Listing from "./models/Listing.js";
+import { oneResult } from "./models/Listing.js";
+
+dotenv.config();
+
+async function connectToMongoDB() {
+  const MongoDBURI = process.env.MONGODB_URI ? process.env.MONGODB_URI : "";
+  await mongoose.connect(MongoDBURI);
+  console.log("connected to mongodb");
+}
 
 const url = "https://sfbay.craigslist.org/sby/sof/7558943157.html";
 
 async function scrapeCraigsList() {
   try {
     const htmlResult = await request.get(url);
+    // console.log(htmlResult);
+    const $ = cheerio.load(htmlResult);
   } catch (error: any) {
     console.log(error.message);
   }
---
+++ models/Listing.ts
@@ -0,0 +1,25 @@
+// TO DO = add address field
+
+import mongoose from "mongoose";
+import { Schema, Document } from "mongoose";
+export interface oneResult extends Document {
+  title: string;
+  datePosted: Date;
+  neighborhood: string;
+  url: string;
+  jobDescription: string;
+  compensation: string;
+}
+
+const listingSchema = new Schema<oneResult>({
+  title: String,
+  datePosted: Date,
+  neighborhood: String,
+  url: String,
+  jobDescription: String,
+  compensation: String,
+});
+
+const Listing = mongoose.model("listings", listingSchema);
+
+export default Listing;
